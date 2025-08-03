a610c3d29baa6e4d13e1678bb6fd9b1ead47dc72
Mcode4 <lamllc20@gmail.com>
2025-08-03 07:42:38 -0400

    grid started

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,4 @@
-import { useState, createContext } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, createContext } from 'react';
 
 export const sizeContext = createContext();
 
--- frontend/src/components/Grid/Grid.jsx
+++ frontend/src/components/Grid/Grid.jsx
@@ -1,9 +1,24 @@
-
+import { useState, useRef } from "react"
+import styles from './Grid.module.css'
 
 function Grid({ height, width }) {
+    const gridRef = useRef(Array.from({length: height}, ()=> Array(width).fill('rgba(25, 0, 255, 0)')));
+    console.log(gridRef.current)
 
     return (
-        <>here now {height}x{width}</>
+        <div className={styles.gridContainer}>
+            {gridRef.current.map((row, x)=> 
+                <div className={styles.row}>
+                {row.map((colColor, x)=> (
+                    <div 
+                        className={styles.pixel}
+                        style={{backgroundColor: `${colColor}`}}
+                    >{colColor}</div>
+                ))}
+                <br />
+                </div>
+            )}
+        </div>
     )
 }
 
